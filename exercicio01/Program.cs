370009674fd5be829742a2f97abaa72e4cc67287
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-29 09:39:54 -0300

    Exercício01

---
+++ exercicio01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercicio01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int contador = 1;
+            int maior18 = 0;
+            Console.WriteLine("Vou contar quantas pessoas tem mais de 18 anos. Insira 10 idades.");
+            do
+            {
+                Console.WriteLine("Digite a idade:");
+                int idade = Convert.ToInt32(Console.ReadLine());
+                if (idade >= 18)
+                {
+                    maior18 += 1;
+                    contador++;
+                }
+                else
+                {
+                    contador++;
+                }
+            } while (contador <= 10);
+            Console.WriteLine($"Existem {maior18} pessoas que tem as idades acima dos 18 anos.");
+        }
+    }
+}
